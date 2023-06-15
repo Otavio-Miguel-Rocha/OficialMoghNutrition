211a085c9b51e9e653377b278713c259f09e4e1a
Otavio-Miguel-Rocha <otaviorochajcm@gmail.com>
2023-06-14 21:53:10 -0300

    Implementa o html e css da lista de pacientes, além do modal com as demais informações.

--- src/app/lista-pacientes/lista-pacientes.component.ts
+++ src/app/lista-pacientes/lista-pacientes.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-lista-pacientes',
-  templateUrl: './lista-pacientes.component.html',
-  styleUrls: ['./lista-pacientes.component.css']
+  selector: "app-lista-pacientes",
+  templateUrl: "./lista-pacientes.component.html",
+  styleUrls: ["./lista-pacientes.component.css"],
 })
 export class ListaPacientesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  upArrowIcon: string;
+  downArrowIcon: string;
+  constructor() {
+    this.upArrowIcon = "/assets/imgs/icons/up-arrow-icon.png";
+    this.downArrowIcon = "/assets/imgs/icons/down-arrow-icon.png";
   }
 
+  ngOnInit() {}
+
+  modal: boolean = true;
+
+  openModal(): void {
+    this.modal = true;
+  }
+  closeModal(): void {
+    this.modal = false;
+  }
 }
