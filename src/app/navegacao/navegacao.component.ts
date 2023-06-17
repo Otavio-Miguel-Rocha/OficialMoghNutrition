679b72e830781790d93165f107e952cab446daa3
gustavogorges <gustavo_gorges@hotmail.com>
2023-06-17 16:11:31 -0300

    implementa revisão de desgin e codigo

--- src/app/navegacao/navegacao.component.ts
+++ src/app/navegacao/navegacao.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegacaoComponent implements OnInit {
 
-  constructor() { }
+  logoMOGH : string;
+
+  constructor() { 
+    this.logoMOGH = '/assets/img/logoMOGH.png'
+  }
 
   ngOnInit() {
   }
